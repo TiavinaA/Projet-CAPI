1ec354c926ac9385ce03acda9593e4e6a69d8557
Marwan <aitaddi.marwan@gmail.com>
2023-12-12 10:34:00 +0100

    Should be ok

--- setup.js
+++ setup.js
@@ -1,13 +1,18 @@
 onload = fInit;
 
-InitForm(){
+function InitForm(){
     const form = document.getElementById("setupForm")
     form.addEventListener("submit", (event) => {
         //On empeche de submit
         event.preventDefault();
+        var settings = Array();
+        settings.push(form["mode"])
+        settings.push(form["tVote"])
+        settings.push(form["tDebat"])
+        localStorage.setItem("settings", JSON.stringify(settings))
     })
 }
 
-function Init(){
-    fInitForm();
+function fInit(){
+    InitForm();
 }
