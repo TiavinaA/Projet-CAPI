12d98566a47ebb9a5e4db1b7a12808ddfc70d944
Woople74 <aitaddi.marwan@gmail.com>
2023-12-26 15:32:02 +0100

    click et sélection sur carte

--- jeu.js
+++ jeu.js
@@ -1,1 +1,15 @@
 /*Gestion de la logique du planning poker*/
+onload = fInit;
+var currentCardId = "none";
+
+function fInitCard(){
+    document.getElementById("cardList").addEventListener("click", (event) => { // On highlight la nouvelle carte et on la garde en mémoire
+        if(currentCardId != "none") document.getElementById(currentCardId).classList.remove("highlighted")
+        currentCardId = event.target.id;
+        document.getElementById(currentCardId).classList.add("highlighted")
+    })
+}
+
+function fInit(){
+    fInitCard();
+}
