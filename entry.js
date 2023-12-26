10652e4191afa0f87e94bb60c9dcf41c307735a7
TiavinaA <tiavinandria@gmail.com>
2023-12-26 15:19:47 +0100

    chargement des backlogs depuis un fichier json

--- entry.js
+++ entry.js
@@ -1,16 +1,14 @@
+
 onload = fInit;
 
-/* */
-function initForm(){
+function initForm() {
     const form = document.getElementById("EntryForm1")
     form.addEventListener("submit", (event) => {
-        //On empeche de submit
         event.preventDefault();
-        //On crée un paragraphe qui contient la tache
         var task = form['newEntryField'].value;
         if (task != "") {
             var taskContainer = document.getElementById("entryList");
-            
+
             var taskElement = document.createElement("p");
             taskElement.classList.add("task");
             taskElement.innerHTML = task;
@@ -19,21 +17,23 @@ function initForm(){
             taskModifButton.value = "supprimer";
             taskModifButton.addEventListener("click", supprTask);
             taskModifButton.classList.add("taskModifButton");
-            
+
             taskElement.appendChild(taskModifButton);
             taskContainer.appendChild(taskElement);
 
             form.reset();
         }
     })
+
     document.getElementById("EntryValidation").addEventListener("click", finishEntryList);
+    document.getElementById("loadJsonButton").addEventListener("click", loadJson);
 }
 
-function supprTask(event){
+function supprTask(event) {
     this.parentNode.parentNode.removeChild(this.parentNode);
 }
 
-function finishEntryList(){
+function finishEntryList() {
     const entryListElement = document.getElementById("entryList");
     var taskArray = Array();
     entryListElement.childNodes.forEach((task) => {
@@ -43,8 +43,50 @@ function finishEntryList(){
     localStorage.setItem("tasks", JSON.stringify(taskArray));
 }
 
+function loadJson() {
+    const fileInput = document.getElementById("fileInput");
 
-function fInit(){
+    if (fileInput.files.length > 0) {
+        const file = fileInput.files[0];
+        const reader = new FileReader();
+
+        reader.onload = function (e) {
+            try {
+                const jsonData = JSON.parse(e.target.result);
+                displayTasksFromJson(jsonData);
+            } catch (error) {
+                console.error('Erreur lors de la lecture du fichier JSON', error);
+            }
+        };
+
+        reader.readAsText(file);
+    } else {
+        console.error('Aucun fichier sélectionné');
+    }
+}
+
+function displayTasksFromJson(tasks) {
+    const taskContainer = document.getElementById("entryList");
+    taskContainer.innerHTML = "";
+
+    tasks.forEach(task => {
+        var taskElement = document.createElement("p");
+        taskElement.classList.add("task");
+        taskElement.innerHTML = task;
+
+        var taskModifButton = document.createElement("input");
+        taskModifButton.type = "button";
+        taskModifButton.value = "supprimer";
+        taskModifButton.addEventListener("click", supprTask);
+        taskModifButton.classList.add("taskModifButton");
+
+        taskElement.appendChild(taskModifButton);
+        taskContainer.appendChild(taskElement);
+    });
+}
+
+function fInit() {
     console.log(localStorage.getItem("tasks"));
     initForm();
 }
+
